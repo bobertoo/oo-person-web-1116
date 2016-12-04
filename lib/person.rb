74beaaf6a324438d63dcd1efcb7efab8dc148fca
Vinny Martinez <vinny@vinnymartinez.com>
2016-12-04 14:15:52 -0500

    sum new changes

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
   attr_accessor :bank_account
-  attr_reader :name, :hygiene, :happiness
+  attr_reader :name, :happiness, :hygiene
 
   def initialize(name)
     @name = name
@@ -17,6 +17,10 @@ class Person
     @hygiene = point_helper(points)
   end
 
+  def point_helper(points)
+    points > 10 ? 10 : (points < 0 ? 0 : points)
+  end
+
   def happy?
     happiness > 7
   end
@@ -26,50 +30,38 @@ class Person
   end
 
   def get_paid(salary)
-    @bank_account += salary
+    self.bank_account += salary
     "all about the benjamins"
   end
 
   def take_bath
-    self.hygiene=(hygiene + 4)
+    self.hygiene += 4
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
   def work_out
-    self.hygiene=(hygiene - 3)
-    self.happiness=(happiness + 2)
+    self.hygiene -= 3
+    self.happiness += 2
     "♪ another one bites the dust ♫"
   end
 
   def call_friend(friend)
-    self.happiness=(happiness + 3)
-    friend.happiness=(friend.happiness + 3)
+    self.happiness += 3
+    friend.happiness += 3
     "Hi #{friend.name}! It's #{name}. How are you?"
   end
 
-  def start_conversation(friend, convo)
-    if convo == "politics"
-      self.happiness=(happiness - 2)
-      friend.happiness=(friend.happiness - 2)
-      "blah blah partisan blah lobbyist"
-    elsif convo == "weather"
-      self.happiness=(happiness + 1)
-      friend.happiness=(friend.happiness + 1)
-      "blah blah sun blah rain"
+  def start_conversation(friend, topic)
+    if topic == "politics"
+      self.happiness -= 2
+      friend.happiness -= 2
+      'blah blah partisan blah lobbyist'
+    elsif topic == "weather"
+      self.happiness += 1
+      friend.happiness += 1
+      'blah blah sun blah rain'
     else
-      "blah blah blah blah blah"
-    end
-  end
-
-  private
-
-  def point_helper(nummy)
-    if nummy > 10
-      nummy = 10
-    elsif nummy < 0
-      nummy = 0
-    else
-      nummy
+      'blah blah blah blah blah'
     end
   end
 end
